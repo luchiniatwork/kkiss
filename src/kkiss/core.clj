4d7d1520a23734bb034a28e98e7d4d8519441d03
Tiago Luchini <tiago@luchini.nyc>
2021-12-12 09:38:08 -0500

    Fix arity issues and add basic failure behavior

--- src/kkiss/core.clj
+++ src/kkiss/core.clj
@@ -21,40 +21,59 @@
        (filter #(= stream-name (:name %)))
        first))
 
+(defn ^:private arity-3? [f]
+  (let [members (-> f
+                    clojure.reflect/reflect
+                    :members)
+        arity-3? (or (= 3 (->> members
+                               (filter #(= 'invoke (:name %)))
+                               first
+                               :parameter-types
+                               count))
+                     (->> members
+                          (filter #(= 'doInvoke (:name %)))
+                          seq))]
+    arity-3?))
+
 (defn consumer
   ([streams handle-fn]
    (consumer streams handle-fn nil))
-  ([streams handle-fn opts]
-   (let [arity-3? (or (>= 2 (->> handle-fn
-                                 clojure.reflect/reflect
-                                 :members
-                                 (filter #(= 'invoke (:name %)))
-                                 :param-types
-                                 count))
-                      (->> handle-fn
-                           clojure.reflect/reflect
-                           :members
-                           (filter #(= 'doInvoke (:name %)))
-                           seq))]
-     (let [wrapper (fn [k v opts]
-                     (let [k-de (-> streams (find-stream stream-name)
-                                    (get-in [:key.serde :deserializer]))
-                           v-de (-> streams (find-stream stream-name)
-                                    (get-in [:value.serde :deserializer]))]
-                       (try
-                         (if arity-3?
-                           (handle-fn (k-de k) (v-de v) opts)
-                           (handle-fn (k-de k) (v-de v)))
-                         (catch Throwable ex
-                           (println "Ignored exeption" ex)
-                           (comment "ignore exception for now")))))]
-       (engine/consumer streams wrapper opts)))))
+  ([streams handle-fn {:keys [continuation
+                              failed-event-stream
+                              failed-exception-stream]
+                       :or {continuation :stop-on-failure}
+                       :as opts}]
+   (let [arity-3?' (arity-3? handle-fn)
+         wrapper (fn [k v {:keys [stream-name] :as opts}]
+                   (let [k-de (-> streams (find-stream stream-name)
+                                  (get-in [:key.serde :deserializer]))
+                         v-de (-> streams (find-stream stream-name)
+                                  (get-in [:value.serde :deserializer]))]
+                     (try
+                       (if arity-3?'
+                         (handle-fn (k-de k) (v-de v) opts)
+                         (handle-fn (k-de k) (v-de v)))
+                       (catch Throwable ex
+                         (when failed-event-stream
+                           (send! failed-event-stream (k-de k) (v-de v)))
+                         (when failed-exception-stream
+                           (send! failed-exception-stream
+                                  (k-de k)
+                                  {:event (v-de v)
+                                   :exception (Throwable->map ex)}))
+                         (if (= :non-stop continuation)
+                           (println "Ignored exception" ex)
+                           (throw ex))))))]
+     (engine/consumer streams wrapper (assoc opts
+                                             :continuation continuation)))))
 
 (defn start! [consumer]
   (engine/start! consumer))
 
 (defn stop! [consumer]
   (engine/stop! consumer))
+
+
 
 
 #_(tests
@@ -70,7 +89,7 @@
                            :value.serde (serde/serde :keyword)})
          c1-visited (atom 0)
          c2-visited (atom 0)
-         c1 (consumer [stream-a] (fn [k v]
+         c1 (consumer [stream-a] (fn [k v {:keys [foobar]}]
                                    (when (and (= k :foo)
                                               (= v :bar))
                                      (swap! c1-visited inc))))
@@ -85,7 +104,8 @@
      (Thread/sleep 50)
      [@c1-visited @c2-visited]) := [1 2]
 
-   "throw does not break consumer"
+
+   "throw breaks consumer by default"
    (let [e (engine {:engine-id :in-memory})
          stream-a (stream {:engine e
                            :name :my-stream-a
@@ -102,11 +122,71 @@
      (send! stream-a :foo :ball)
      (send! stream-a :foo :bar)
      (Thread/sleep 50)
+     @c-visited) := 1
+
+
+   "throw does not stops consumer in case the consumer wants it"
+   (let [e (engine {:engine-id :in-memory})
+         stream-a (stream {:engine e
+                           :name :my-stream-a
+                           :key.serde (serde/serde :keyword)
+                           :value.serde (serde/serde :keyword)})
+         c-visited (atom 0)
+         c (consumer [stream-a] (fn [k v]
+                                  (if (and (= k :foo)
+                                           (= v :bar))
+                                    (swap! c-visited inc)
+                                    (throw (ex-info "invalid msg" {}))))
+                     {:continuation :non-stop})]
+     (start! c)
+     (send! stream-a :foo :bar)
+     (send! stream-a :foo :ball)
+     (send! stream-a :foo :bar)
+     (Thread/sleep 50)
      @c-visited) := 2
 
-   
-   )
 
+   "throw does not break consumer but also send event to another
+   stream and exception to another one"
+   (let [e (engine {:engine-id :in-memory})
+         stream-a (stream {:engine e
+                           :name :my-stream-a
+                           :key.serde (serde/serde :keyword)
+                           :value.serde (serde/serde :keyword)})
+         failed-stream (stream {:engine e
+                                :name :failed-stream
+                                :key.serde (serde/serde :keyword)
+                                :value.serde (serde/serde :keyword)})
+         ex-stream (stream {:engine e
+                            :name :ex-stream
+                            :key.serde (serde/serde :keyword)
+                            :value.serde (serde/serde :keyword)})
+         c-visited (atom 0)
+         c (consumer [stream-a] (fn [k v]
+                                  (if (and (= k :foo)
+                                           (= v :bar))
+                                    (swap! c-visited inc)
+                                    (throw (ex-info "invalid msg" {}))))
+                     {:continuation :non-stop
+                      :failed-event-stream failed-stream
+                      :failed-exception-stream ex-stream})
+         failed-c-out (atom {})
+         failed-c (consumer [failed-stream ex-stream]
+                            (fn [k v {:keys [stream-name]}]
+                              (swap! failed-c-out assoc
+                                     stream-name {:k k
+                                                  :result v})))]
+     (start! c)
+     (start! failed-c)
+     (send! stream-a :foo :bar)
+     (send! stream-a :foo :ball)
+     (send! stream-a :foo :bar)
+     (Thread/sleep 50)
+     [@c-visited
+      (= :ball (-> @failed-c-out :failed-stream :result))
+      (= :ball (-> @failed-c-out :ex-stream :result :event))
+      (-> @failed-c-out :ex-stream :result :exception :cause)]) := [2 true true "invalid msg"]
+   )
 
 
 
@@ -132,10 +212,10 @@
                             :config {"client.id" "my-producer"
                                      "acks" "all"}}))
 
-  (send! test-stream :k :v6666)
+  (send! test-stream :k :v677)
   
   (def c3 (consumer [test-stream]
-                    (fn [k v] (println stream k v))
+                    (fn [k v] (println k v))
                     {:polling-timeout 2000
                      :config {"auto.offset.reset" "earliest"
                               "enable.auto.commit" true
@@ -145,4 +225,36 @@
 
   (stop! c3)
 
+;;;; exception
+
+  ;; never commits
+  (def c4 (consumer [test-stream]
+                    (fn [k v]
+                      (println k v))
+                    {:polling-timeout 2000
+                     :kkiss.engine.kafka/commit-behavior :do-not-manually-commit
+                     :config {"auto.offset.reset" "earliest"
+                              "enable.auto.commit" false
+                              "group.id"           "my-group6"}}))
+
+  (start! c4)
+
+  (stop! c4)
+
+
+  (def c5 (consumer [test-stream]
+                    (fn [k v]
+                      (when (= :v7777 v)
+                        (throw (ex-info "what is this?" {})))
+                      (println k v))
+                    {:polling-timeout 200
+                     :config {"auto.offset.reset" "earliest"
+                              "enable.auto.commit" false
+                              "group.id"           "my-group15"}}))
+
+  (start! c5)
+
+  (stop! c5)
+
+  
   )
